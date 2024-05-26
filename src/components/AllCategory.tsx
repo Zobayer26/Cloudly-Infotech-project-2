b427133fda206c19f3893f283281ddac7cbfed28
Zobayer26 <zobayerarif126@gmail.com>
2024-05-27 00:04:58 +0600

    functional filter category

--- src/components/AllCategory.tsx
+++ src/components/AllCategory.tsx
@@ -3,7 +3,7 @@
 import { Rating } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 import Banner from "./Banner";
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
@@ -48,11 +48,13 @@ type productDataType = {
 }
 
 const AllCategory = () => {
+
     const [data, setData] = useState<DataItem[]>([])
     const [smartphoneData, setSmartPhoneData] = useState<productDataType[]>()
     const [laptoptData, setLaptoptData] = useState<productDataType[]>()
     const [groceriesData, setGroceriesData] = useState<productDataType[]>()
-    const [isOpen, setIsOpen] = useState(false)
+    const [filterCategory, setFilterCategory] = useState<string>("All")
+    const [filterData, setFilterData] = useState<productDataType[]>()
     const router = useRouter()
 
     useEffect(() => {
@@ -87,6 +89,17 @@ const AllCategory = () => {
         fetchGroceriesData();
     }, [])
 
+    const filterProductCategory = useCallback((category: string) => {
+
+        async function fetchDataByCategory() {
+            const response = await fetch(`https://dummyjson.com/products/category/${category}`)
+            const value = await response.json()
+            setFilterData(value.products)
+
+        }
+
+        fetchDataByCategory();
+    }, [])
 
     const handleClick: any = (id: number) => {
         router.push(`/Products/${id}`)
@@ -99,10 +112,16 @@ const AllCategory = () => {
     return (
         <div className="flex gap-[20px]">
             <div className=" hidden md:flex flex-col gap-4 px-2">
-                <h1 className=" text-xl font-semibold mt-[20px]">All categories</h1>
+                <h1 onClick={()=>setFilterCategory("All")}
+                 className=" text-xl font-semibold mt-[20px] cursor-pointer">All categories</h1>
                 <div className="flex flex-col gap-[10px]">
                     {data.map((item) => (
-                        <div key={item.slug} className=" cursor-pointer">
+                        <div key={item.slug}
+                            onClick={() =>{
+                                setFilterCategory(item.slug)
+                                filterProductCategory(item.slug)
+                            }}
+                            className=" cursor-pointer">
                             {item.slug}
                         </div>
                     ))}
@@ -117,10 +136,17 @@ const AllCategory = () => {
                 </Button>
                 <Drawer open={open} onClose={toggleDrawer(false)}>
                     <div className=" w-[400px] bg-[#1b6392]  text-white px-[40px] py-10">
-                        <h1 className=" capitalize">All category</h1>
+                        <h1  onClick={() =>{
+                                setFilterCategory("All")
+                            }}
+                         className=" capitalize">All category</h1>
                         <div className="px-5 py-3 flex flex-col gap-[10px]">
                             {data.map((item) => (
-                                <div key={item.slug} className=" px-3 py-2  rounded-md  transition-all duration-300
+                                <div  onClick={()=>{
+                                    setFilterCategory(item.slug)
+                                    filterProductCategory(item.slug)
+                                }}
+                                key={item.slug} className=" px-3 py-2  rounded-md  transition-all duration-300
                                  hover:bg-[#383838] hover:shadow-md cursor-pointer">
                                     {item.slug}
                                 </div>
@@ -130,80 +156,116 @@ const AllCategory = () => {
                 </Drawer>
 
             </div>
-            <div className="flex-grow">
-                <h1 className="ml-[20px] font-[700] text[26px] leading-[30px]
+            {
+                filterCategory === 'All' ?
+                    <>
+                        <div className="flex-grow">
+                            <h1 className="ml-[20px] font-[700] text[26px] leading-[30px]
                  my-[20px]">Smart Phones</h1>
-                <div className=" grid grid-cols-1 md:grid-cols-4">
-                    {
-                        smartphoneData?.slice(0, 4).map((product) => (
-                            <div onClick={() => handleClick(product.id)}
-                                className=" flex flex-col justify-center items-center gap-[10px] cursor-pointer"
-                                key={product.id}>
-                                <div className="w-[350px] h-[350px]">
-                                    <Image src={product.images[0]} alt=""
-                                        className="w-full h-full  object-contain" width={300} height={300} />
-                                </div>
-                                <div>
-                                    <Rating name="halfefau-rating-read" value={product.rating}
-                                        precision={0.1} readOnly />
-                                    <h1 className="font-[600]">{product.title}</h1>
-                                    < p className="text-[30px] font-[700] text-[#47b0f5] ">
-                                        $ {product.price}
-                                    </p>
-                                </div>
+                            <div className=" grid grid-cols-1 md:grid-cols-4">
+                                {
+                                    smartphoneData?.slice(0, 4).map((product) => (
+                                        <div onClick={() => handleClick(product.id)}
+                                            className=" flex flex-col justify-center items-center gap-[10px] cursor-pointer"
+                                            key={product.id}>
+                                            <div className="w-[350px] h-[350px]">
+                                                <Image src={product.images[0]} alt=""
+                                                    className="w-full h-full  object-contain" width={300} height={300} />
+                                            </div>
+                                            <div>
+                                                <Rating name="halfefau-rating-read" value={product.rating}
+                                                    precision={0.1} readOnly />
+                                                <h1 className="font-[600]">{product.title}</h1>
+                                                < p className="text-[30px] font-[700] text-[#47b0f5] ">
+                                                    $ {product.price}
+                                                </p>
+                                            </div>
 
+                                        </div>
+                                    ))
+                                }
                             </div>
-                        ))
-                    }
-                </div>
-                <Banner />
-                <h1 className="ml-[20px] font-[700] text[26px] leading-[30px] my-[20px]">Laptop</h1>
-                <div className=" grid grid-cols-1 md:grid-cols-4">
-                    {
-                        laptoptData?.slice(0, 4).map((product) => (
-                            <div onClick={() => handleClick(product.id)}
-                                className=" flex flex-col justify-center items-center gap-[10px] cursor-pointer"
-                                key={product.id}>
-                                <div className="w-[350px] h-[300px] max-w-[300px] max-h-[300px]">
-                                    <Image src={product.images[0]} alt="" className="w-full h-full" width={300} height={300} />
-                                </div>
-                                <div>
-                                    <Rating name="halfefau-rating-read" value={product.rating}
-                                        precision={0.1} readOnly />
-                                    <h1 className="font-[600]">{product.title}</h1>
-                                    < p className="text-[30px] font-[700] text-[#47b0f5] ">
-                                        $ {product.price}
-                                    </p>
-                                </div>
+                            <Banner />
+                            <h1 className="ml-[20px] font-[700] text[26px] leading-[30px] my-[20px]">Laptop</h1>
+                            <div className=" grid grid-cols-1 md:grid-cols-4">
+                                {
+                                    laptoptData?.slice(0, 4).map((product) => (
+                                        <div onClick={() => handleClick(product.id)}
+                                            className=" flex flex-col justify-center items-center gap-[10px] cursor-pointer"
+                                            key={product.id}>
+                                            <div className="w-[350px] h-[300px] max-w-[300px] max-h-[300px]">
+                                                <Image src={product.images[0]} alt="" className="w-full h-full" width={300} height={300} />
+                                            </div>
+                                            <div>
+                                                <Rating name="halfefau-rating-read" value={product.rating}
+                                                    precision={0.1} readOnly />
+                                                <h1 className="font-[600]">{product.title}</h1>
+                                                < p className="text-[30px] font-[700] text-[#47b0f5] ">
+                                                    $ {product.price}
+                                                </p>
+                                            </div>
 
+                                        </div>
+                                    ))
+                                }
                             </div>
-                        ))
-                    }
-                </div>
-                <h1 className="ml-[20px] font-[700] text[26px] leading-[30px] my-[20px]">Groceries</h1>
-                <div className=" grid grid-cols-1 md:grid-cols-4">
-                    {
-                        groceriesData?.slice(0, 4).map((product) => (
-                            <div onClick={() => handleClick(product.id)}
-                                className=" flex flex-col justify-center items-center gap-[10px] cursor-pointer"
-                                key={product.id}>
-                                <div className="w-[350px] h-[300px] max-w-[300px] max-h-[300px]">
-                                    <Image src={product.images[0]} alt="" className="w-full h-full" width={300} height={300} />
-                                </div>
-                                <div>
-                                    <Rating name="halfefau-rating-read" value={product.rating}
-                                        precision={0.1} readOnly />
-                                    <h1 className="font-[600]">{product.title}</h1>
-                                    < p className="text-[30px] font-[700] text-[#47b0f5] ">
-                                        $ {product.price}
-                                    </p>
-                                </div>
+                            <h1 className="ml-[20px] font-[700] text[26px] leading-[30px] my-[20px]">Groceries</h1>
+                            <div className=" grid grid-cols-1 md:grid-cols-4">
+                                {
+                                    groceriesData?.slice(0, 4).map((product) => (
+                                        <div onClick={() => handleClick(product.id)}
+                                            className=" flex flex-col justify-center items-center gap-[10px] cursor-pointer"
+                                            key={product.id}>
+                                            <div className="w-[350px] h-[300px] max-w-[300px] max-h-[300px]">
+                                                <Image src={product.images[0]} alt="" className="w-full h-full" width={300} height={300} />
+                                            </div>
+                                            <div>
+                                                <Rating name="halfefau-rating-read" value={product.rating}
+                                                    precision={0.1} readOnly />
+                                                <h1 className="font-[600]">{product.title}</h1>
+                                                < p className="text-[30px] font-[700] text-[#47b0f5] ">
+                                                    $ {product.price}
+                                                </p>
+                                            </div>
 
+                                        </div>
+                                    ))
+                                }
                             </div>
-                        ))
-                    }
-                </div>
-            </div>
+                        </div>
+                    </> :
+                    <>
+                        <div className="flex-grow">
+                            <h1 className="ml-[20px] font-[700] text[26px] leading-[30px]
+                 my-[20px]">{filterCategory}</h1>
+                            <div className=" grid grid-cols-1 md:grid-cols-4">
+                                {
+                                    filterData?.map((product) => (
+                                        <div onClick={() => handleClick(product.id)}
+                                            className=" flex flex-col justify-center items-center gap-[10px] cursor-pointer"
+                                            key={product.id}>
+                                            <div className="w-[350px] h-[350px]">
+                                                <Image src={product.images[0]} alt=""
+                                                    className="w-full h-full  object-contain" width={300} height={300} />
+                                            </div>
+                                            <div>
+                                                <Rating name="halfefau-rating-read" value={product.rating}
+                                                    precision={0.1} readOnly />
+                                                <h1 className="font-[600]">{product.title}</h1>
+                                                < p className="text-[30px] font-[700] text-[#47b0f5] ">
+                                                    $ {product.price}
+                                                </p>
+                                            </div>
+
+                                        </div>
+                                    ))
+                                }
+                            </div>
+
+                        </div>
+                    </>
+            }
+
         </div>
     )
 }
